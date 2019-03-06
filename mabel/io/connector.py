b15f5d5228246a26dd06427fa71691490518d171
AlessioZanga <alessio.zanga@outlook.it>
2019-03-06 14:10:50 +0100

    Add ZipConnector

--- mabel/io/connector.py
+++ mabel/io/connector.py
@@ -1,4 +1,5 @@
 import os
+import zipfile
 from abc import ABC, abstractmethod
 
 
@@ -27,4 +28,21 @@ class FolderConnector(DataConnector):
         path = os.path.join(self._path, path)
         with open(path, 'rb') as binaryFile:
             file = binaryFile.read()
+        return file
+
+
+class ZipConnector(DataConnector):
+
+    def __init__(self, path):
+        super().__init__(path)
+
+    def getFilesList(self):
+        with zipfile.ZipFile(self._path) as archive:
+            files = archive.namelist()
+        return files
+
+    def getFile(self, path):
+        with zipfile.ZipFile(self._path) as archive:
+            file = archive.open(path)
+            file = file.read()
         return file
